ef561085fad9804a98d83a2d69810a537ad81f50
shpark <jlk@shpark.local>
2020-09-27 19:30:04 +0900

    관심사 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,7 +3,6 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 
@@ -13,7 +12,8 @@ public class MemberApp {
         *  1. 다른 저장소로 변경할 때 OCP(개방-폐쇄 원칙)을 지킬 수 없다.
         *  2. DIP(의존 역전 원칙) - 의존 관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있다.
         * */
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,29 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /**
+     * RateDiscountPolicy를 적용시키기 위해 OrderServiceImpl를 수정해야 하므로 DIP 위반
+     * 기능을 확장해서 코드를 변경하게 되면 클라이언트 코드에 영향을 주게 되므로 OCP 위반
+     *
+     * DIP를 위반하지 않도록 인터페이스에만 의존하도록 의존관계 변경 필요
+     * OrderServiceImpl에 DiscountPolicy 구현 객체를 생성하고 주입해주어야 함
+     * 주입될 구현 객체는 외부(AppConfig)에서 결정
+     * OrderServiceImpl은 의존 관계에 대한 고민을 할 필요없이 실행에만 집중할 수 있음
+     */
+
+    private final MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
