52886c60904f47930551943ed51f13138e5ecd01
seon54 <seonhyeongpark54@gmail.com>
2020-10-01 22:01:48 +0900

    @Configuration과 바이트코드 조작

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,12 @@ public class ConfigurationSingletonTest {
         assertThat(memberRepository1).isSameAs(memberRepository2);
         assertThat(memberRepository).isSameAs(memberRepository2);
     }
+    
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
 }
